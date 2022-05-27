9f455b08d1963a798fe591545a25138d2804a631
wjddns4003 <wjddns0882@gmail.com>
2022-05-27 14:31:40 +0900

    Build : 프로젝트 추가 - 이정운

---
+++ src/main/java/com/highlife/rainbow/RainbowApplication.java
@@ -0,0 +1,13 @@
+package com.highlife.rainbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowApplication.class, args);
+	}
+
+}
