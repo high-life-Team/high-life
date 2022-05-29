4fc177c7b0daff5eadc6d056cf0c1e99c279d87f
taehyeok <ajaj3122@naver.com>
2022-05-30 00:43:09 +0900

    Feat : 도메인 Entity 추가 - 설태혁

---
+++ src/main/java/com/highlife/rainbow/domain/Emotion.java
@@ -0,0 +1,42 @@
+package com.highlife.rainbow.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class , property = "id")
+public class Emotion {
+	@Id
+	@Column(name = "emotion_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+    // **** 연관관계 정리 ****
+	@OneToOne
+	@JoinColumn(name = "member_id", referencedColumnName = "member_id")
+	private Member member;
+	
+	@OneToOne
+	@JoinColumn(name = "board_id", referencedColumnName = "board_id")
+	private Board board;
+	
+	
+	private int love;
+	private int sad;
+	private int cheerUp;
+	private int angry;
+	private int total; // 위 4가지 감정 종합 cnt
+	
+}
---
+++ src/main/java/com/highlife/rainbow/domain/Reply.java
@@ -0,0 +1,63 @@
+package com.highlife.rainbow.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class , property = "id")
+public class Reply {
+	
+	@Id
+	@Column(name = "reply_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "board_id", referencedColumnName = "board_id")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name = "member_id", referencedColumnName = "member_id")
+	private Member member;
+	
+	private String content;
+	
+	
+    @Column(updatable = false) // update된 시점에 기존에 저장되어 있던 데이터를 유지
+    @CreationTimestamp
+	private LocalDateTime regDate;
+	
+	@Column(name = "reply_hide")
+	private boolean hide;
+	
+	private int reportReason;
+	
+	private int love;
+	
+	
+    // **** 연관관계 정리 ****
+	@OneToMany(mappedBy = "reply")
+	private List<ReplyReport> reportes;
+	
+	
+	
+}
