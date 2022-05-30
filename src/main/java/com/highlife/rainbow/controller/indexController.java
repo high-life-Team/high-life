3f9f82dd7a067c0096c1fa943c7f5a154e947370
yuuulya <yla101@naver.com>
2022-05-30 17:42:11 +0900

    chore : 충돌 해결  - 율리아

--- src/main/java/com/highlife/rainbow/controller/indexController.java
+++ src/main/java/com/highlife/rainbow/controller/indexController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping()
+	@GetMapping("/")
     public String main(){
        return "index";
     }
+	
 }
--- src/main/java/com/highlife/rainbow/controller/memberController.java
+++ src/main/java/com/highlife/rainbow/controller/memberController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class memberController {
 
-	@GetMapping("/")
-    public String main(){
-       return "index";
+	@GetMapping("login")
+    public String log(){
+       return "login";
     }
-	
 }
