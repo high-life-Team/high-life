954286a01da3109ecefe413c12e31a51aad9810d
euntaek419 <cet4713@gmail.com>
2022-06-02 15:23:35 +0900

    Feat : 회원가입 기능 구현 - 최은택

--- src/main/java/com/highlife/rainbow/controller/MemberController.java
+++ src/main/java/com/highlife/rainbow/controller/MemberController.java
@@ -1,7 +1,20 @@
 package com.highlife.rainbow.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.highlife.rainbow.member.MemberDTO;
+import com.highlife.rainbow.service.MemberService;
 
 @Controller
 public class MemberController {
@@ -11,19 +24,25 @@ public class MemberController {
 		
        return "member";
     }
-    
-    
-    
 	
-//	@RequestMapping(value = "memberinsert", method = RequestMethod.GET)
-//	public String insertform() throws Exception {
-//		return "login";
-//	}
-
-
-//	@RequestMapping(value = "memberinsert", method = RequestMethod.POST)
-//	public String insertresult(MemberDTO dto) throws Exception {
-//		int result = service.memberinsert(dto); //dto의 값 가져옴
-//			System.out.println(result);
-//		return "redirect:login";
+	@Autowired
+	@Qualifier("memberservice")
+	MemberService service;
+	
+	@RequestMapping(value = "/memberinsert", method = RequestMethod.GET)
+	public String insertform() throws Exception {
+		return "/member";
+	}
+	
+	
+	@RequestMapping(value = "/memberinsert", method = RequestMethod.POST)
+	public String insertresult(MemberDTO dto) throws Exception {
+		int result = service.memberinsert(dto);// id, email, phone �ߺ� �Ұ���
+		System.out.println(result);
+		return "redirect:login";
+	}
+	
+	
+	
 }
+
---
+++ src/main/java/com/highlife/rainbow/service/MemberServiceImpl.java
@@ -0,0 +1,37 @@
+package com.highlife.rainbow.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.highlife.rainbow.member.MemberDAO;
+import com.highlife.rainbow.member.MemberDTO;
+
+
+@Service("memberservice")
+public class MemberServiceImpl implements MemberService {
+	
+	@Autowired
+	@Qualifier("memberDAO")
+	MemberDAO dao;
+
+	@Override
+	public int memberinsert(MemberDTO dto) {
+		return dao.memberinsert(dto);
+	}
+
+	
+	@Override
+	public MemberDTO login(MemberDTO memberdto) {
+		return dao.login(memberdto);
+	}
+
+	
+
+	
+	 
+
+
+}
+
+
