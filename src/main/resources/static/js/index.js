bd4ceac0c080987a6cf9ba40ca68087299344c4a
yuuulya <yla101@naver.com>
2022-06-03 11:17:23 +0900

    feat : index 풀스크린 형식으로 디자인 변경 - 율리아

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -15,3 +15,49 @@ $(".box_content").each(function () {
         }
     });
 });
+
+$(".main_scroll").click(function(event){            
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
+});
+
+var textWrapper = document.querySelector('.main_title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.main_title .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.main_title',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
+  var textWrapper = document.querySelector('.sub_title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.sub_title .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.sub_title',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+
+
