a9c891985cacc7a4dae8183820a69222b3fc4a12
me <melangegrayletter@gmail.com>
2022-06-08 00:16:09 +0900

    Feat : 글쓰기페이지 모달 작업 - 희수

---
+++ src/main/resources/static/js/writeform.js
@@ -0,0 +1,20 @@
+//writeform modal
+$(document).on('click', '.writeform_btn ', (function () {
+ 
+ 
+ $("#modal_writeform").show();
+
+}));
+
+//writeform modal
+$(document).on('click', '#modal_writeform_close_btn', (function () {
+	
+	  
+$("#modal_writeform").hide();
+
+
+}));
+
+
+	
+	
